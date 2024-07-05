778eb0b6fdb6b31838bb445b50cf0ab302be50eb
ana <acantarellif@gmail.com>
2024-07-05 12:28:26 -0300

    atualizar

---
+++ src/components/Lista.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+function Lista() {
+    let [tarefa, setTarefas] = useState(0);
+    let [novaTarefa, setNovaTarefas] = useState('');
+    
+    const adicionarTarefa = () => {
+        if (novaTarefa.trim() !== '') {
+            setTarefas([...setTarefas, novaTarefa]);
+            setNovaTarefas('');
+        }
+    };
+
+    return(
+        <>
+            <input type="text" value={novaTrefa} onChange={(e) => setNovaTarefas(e.target.value)} placeholder="Digite uma nova tarefa" />
+            <button onClick={adicionarTarefa}>
+                AdicionarTarefa
+            </button>
+            <ul>
+                {tarefa.map((tarefa,index) => (<li key={index>{tarefa}}></li>))}
+            </ul>
+        </> //mesma coisa que uma div
+        
+    );
+}
+
+export default Lista;
---
+++ src/components/Textos.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+function Textos() {
+    const [mostrarTexto, setMostrarTexto] = useState(true);
+
+    const alternarTexto = () => {
+        setMostrarTexto(!mostrarTexto);
+    };
+    
+
+    return(
+        <>
+            <button onClick={alternarTexto}>Mudar o texto</button>
+            {mostrarTexto ? <p>Texto A</p> : <p>Texto B</p>} 
+        </> //mesma coisa que uma div
+    );
+}
+
+export default Textos;
